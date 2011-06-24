49772590f3186c02eed1ebee6dd7b8d47e75ad36
hongkehong <hongkezhu@yahoo.com>
2011-06-23 18:32:20 -0700

    delete unused test/unit, add some layout, add+delete=nothing happend

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def action1
+	@title="action1"
   end
 
   def action2
+   @title="action2"
   end
 
 end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe PagesController do
+	integrate_views
+  #Delete these examples and add some real ones
+  #it "should use PagesController" do
+  #  controller.should be_an_instance_of(PagesController)
+  #end
+
+
+  describe "GET 'action1'" do
+    it "should be successful" do
+      get 'action1'
+      response.should be_success
+    end
+    it "should have the right title" do
+       get 'action1'
+       response.should have_tag("title",
+                           "HAHA | action1")
+	 end
+  end
+
+  describe "GET 'action2'" do
+    it "should be successful" do
+      get 'action2'
+      response.should be_success
+    end
+  end
+end
